453c493d9075210f64e515cf8323a6ec8e5ecb0f
SharonWendoh <swendoh.sharon@gmail.com>
2022-10-20 15:14:56 +0300

    added files

---
+++ homepage.php
@@ -0,0 +1,212 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TREATS BY WENDOH</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <script 
+        src="https://kit.fontawesome.com/22384b239a.js" crossorigin="anonymous">
+    </script>
+</head>
+<body>
+    
+    <header>
+        <div class="wrapper"> 
+            <div class="logo">
+                <img src="images/2021-07-04 (2).png" alt="">
+                <p></p>
+
+            </div>
+            <div>
+                
+                <ul class="navigation-area" id="navLinks">
+                    <li><a href="homepage.php">HOME</a></li>
+                    <li><a href="login.php">LOGIN</a></li>
+                    <li><a href="menu.php">MENU</a></li>
+                    <li><a href="#">TREAT BOXES & SPECIALS</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="#">FEEDBACK</a></li>
+                    <li>
+                        <div class="user-wrapper">
+                            <img src="images/IMG_1827.JPG" width="40px" height="40px" alt="">
+                            <div>
+                                
+                                
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
+        
+        <div class="welcome-text"> 
+            <h1>A treat a day keeps the stress away.</h1>
+            <p>Obey all your sweet tooth desires.</p>
+            <a class="button" href="menu.php">Order today</a>
+        </div>
+        
+    </header>
+
+    <section class="treats">
+        <h1>Our delicious treats</h1>
+        <p></p>
+
+        <div class="row">
+            <div class="treats-col">
+                <img src="images/butterfly cake.jpg">
+                <div class="layer">
+                    <h3>Cakes</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/snow cupcakes.jpg">
+                <div class="layer">
+                    <h3>Cupcakes</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/rasberry cakesickles.jpg">
+                <div class="layer">
+                    <h3>cakesickles</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/beautiful strawberry dips.jpg">
+                <div class="layer">
+                    <h3>Strawberry dips</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="treats-col">
+                <img src="images/love heart sugar cookies.jpg">
+                <div class="layer">
+                    <h3>Sugar cookies</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/purple macaroons.jpg">
+                <div class="layer">
+                    <h3>Macaroons</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/donuts.jpg">
+                <div class="layer">
+                    <h3>Donuts</h3>
+                </div>
+            </div>
+            <div class="treats-col">
+                <img src="images/champaign treatbox.jpg">
+                <div class="layer">
+                    <h3>treatboxes</h3>
+                </div>
+            </div>
+    </section>
+
+    <section class="testimonials">
+        <h1>What Our Customers Say</h1>
+        <p>Over the years, our customers have left very positive and encouraging feedback</p>
+
+        <div class="row">
+            <div class="testomonial-col">
+                <img src="images/IMG_1827.JPG">
+                <div>
+                    <p>I trusted treats by wendoh to do my wedding's dessert table and my oh my, it was the best decision i had ever made. The treats were so delicious that everyone kept on asking me for the contact of whoever did my dessert </p>
+                    <h3>Sharon Wendoh</h3>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star-o" ></i>
+                </div>
+            </div>
+            <div class="testomonial-col">
+                <img src="images/user1.jfif">
+                <div>
+                    <p>Treats by Wendoh treatboxes are just the best. I ordered one for the love of my life during my proposal and she said YES! </p>
+                    <h3>Paul Otieno</h3>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star" ></i>
+                    <i class="fa fa-star-half"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="Partners">
+        <h1>Our Partners</h1>
+        <p>We are greatful to the following companies for their partnership and support.</p>
+        <div class="row">
+            <div class="partners-col">
+                <img src="images/ole sereni.jfif">
+            </div>
+            <div class="partners-col">
+                <img src="images/kempinski.png">
+            </div>
+            <div class="partners-col">
+                <img src="images/the social house.jfif">
+            </div>
+
+        </div>
+    </section>
+
+    <section class="cta">
+        <h1>Order any of our treats and sweet delicasies<br> from any part of Nairobi.</h1>
+        <a class="button" href="">CONTACT US</a>
+
+    </section>
+
+    <section class="footer">
+        <h4>About Us</h4>
+        <p>Treats by Wendoh started as an online baking shop during the pandemic.<br>Thanks to your support it was able to grow into a big bakery that is known nationwide.<br>This even enabled us to start a physical shop in the cbd which is really flourishing. <br>we are looking forward to start other branches in other counties such as Mombasa, Kisumu,Machakos e.t.c.</p>
+        <div class="icons">
+            <i class="fa fa-facebook" ></i>
+            <i class="fa fa-twitter" ></i>
+            <i class="fa fa-instagram" ></i>
+            <i class="fa fa-pinterest" ></i>
+        </div>
+        
+
+        <hr class="seperator">
+        <h4>MORE</h4>
+
+        <div class="row">
+            <div class="footer-col">
+                <h4>Location</h4>
+                <p>Bihi Towers,</p>
+                <p>Moi Avenue,</p>
+                <p>Nairobi, CBD.</p>
+                <p>Tel 0704448418.</p>
+                <p>Whatsapp 0704448418.</p>
+            </div>
+            <div class="footer-col">
+                <h4>Extras</h4>
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Cookie Policy</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Gifts</h4>
+                <ul>
+                    <li><a href="#">Newsletter Signup</a></li>
+                    <li><a href="#">Get Coupons</a></li>
+                    <li><a href="#">Purchase gift vouchers</a></li>
+                </ul>
+            </div>
+        </div>
+        <p>Made with <i class="fa fa-heart-o" ></i> by Sharon Wendoh.</p>
+    </section>
+    
+   
+</body>
+</html>
